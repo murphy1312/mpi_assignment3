839fd03116c5a1abc13e53c74f51f18d00ddb8e2
murphy1312 <murphy@woweed.de>
2016-04-16 17:36:43 +0100

    broadcast array

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,10 +8,12 @@
 #include <stdlib.h>   
 #include <cstdlib>
 #include <cmath>
+#include <cstdio>
 
 
 
 int world_rank;
+
 
 double fRand(double fMin, double fMax)
 {
@@ -19,44 +21,69 @@ double fRand(double fMin, double fMax)
 	return fMin + f * (fMax - fMin);
 }
 
-template <size_t sizeRow, size_t sizeColumn>
-void initIntMatrix(int(&arr)[sizeRow][sizeColumn])
+// template <size_t sizeRow, size_t sizeColumn>
+void initIntMatrix(int** arr, int matrix_size)
 {
-	for (int i = 0; i < sizeRow; i++)
+	for (int i = 0; i < matrix_size; i++)
 	{
-		for (int j = 0; j < sizeColumn; j++)
+		for (int j = 0; j < matrix_size; j++)
 		{
-			arr[i][j] = 1;
-				//(int) fRand(0,50);
+			arr[i][j] = (int) fRand(0,10);
+				
 		}
 	}
 }
 
 #pragma warning (disable : 4996) // visual studio does not compile fopen because of security  
-void printMatrixFromFile() 
+template <typename Type>
+void printMatrixFromFile(Type** arr, int size)
 {
-	double matrix[64];
-	FILE *input = fopen("matrix.dat", "rd");
-	if (!input) {
+	double* buffer;
+	long lSize;
+	size_t result;
+
+	// FILE *input;
+	// fopen_s(&input,"matrix.dat", "rb");
+
+	FILE *input = fopen("matrix.txt", "rb");
+	if (!input) 
+	{
 		return;
 	}
-	fread(matrix, sizeof(double), 64, input);
+
+	// obtain file size:
+	fseek(input, 0, SEEK_END);
+	lSize = ftell(input);
+	rewind(input);
+
+	// allocate memory to contain the whole file:
+	buffer = (double*) malloc(sizeof(double)*lSize);
+
+	std::cout << "printMatrixFromFile sizeof(double) " << sizeof(double) << " ";
+	std::cout << "printMatrixFromFile size*size " << (size*size) << " " << std::endl;
+
+
+	result = fread_s(buffer, lSize, sizeof(double), size*size, input);
+	
 	fclose(input);
-	for (int i = 0; i < 64; ++i) {
-		if (i % 8 == 0)
-			std::cout << std::endl;
-		else
-			std::cout << matrix[i] << " ";
+	for (int i = 0; i < size*size; i++)
+	{
+		std::cout << buffer[i] << " ";
+		if( (i+1) % size == 0)
+		{
+			std::cout <<  std::endl;
+		}	
 	}
+
 }
 
 // print a 2 dimensional array
-template <typename Type, size_t sizeRow, size_t sizeColumn>
-void printMatrix(Type(&arr)[sizeRow][sizeColumn])
+template <typename Type>
+void printMatrix(Type** arr)
 {
-	for (int i = 0; i < sizeRow; i++) 
+	for (int i = 0; i < matrix_size; i++)
 	{
-		for (int j = 0; j < sizeColumn; j++)
+		for (int j = 0; j < matrix_size; j++)
 		{
 			std::cout << arr[i][j] << " ";
 		}
@@ -66,35 +93,12 @@ void printMatrix(Type(&arr)[sizeRow][sizeColumn])
 }
 
 // returns the dot product of two int matricies
-template <size_t sizeRowA, size_t sizeColumnA, size_t sizeRowB, size_t sizeColumnB>
-int dotProduct_two_dimensional(int(&matrixA)[sizeRowA][sizeColumnA], int row, int(&matrixB)[sizeRowB][sizeColumnB], int column)
+int dotProduct(int* arrayA, int** arrayB, int column, int matrix_size)
 {
-	// only works if the size of the row of A is equal to size of column of B
-	if(sizeRowA != sizeColumnB)
-	{
-		return EXIT_FAILURE;
-	}
-	int result = 0;
-	for (size_t i = 0; i < sizeColumnA; i++)
-	{
-		result += matrixA[row][i] * matrixB[i][column];
-	}
-	return result;
-}
-
-// returns the dot product of two int matricies
-template <size_t sizeRowA, size_t sizeRowB, size_t sizeColumnB>
-int dotProduct(int(&arrayA)[sizeRowA], int(&arrayB)[sizeRowB][sizeColumnB], int column)
-{
-	// only works if the size of the row of A is equal to size of column of B
-	if (sizeRowA != sizeColumnB)
-	{
-		return EXIT_FAILURE;
-	}
 
 	int result = 0;
 
-	for (int i = 0; i < sizeRowA; i++)
+	for (int i = 0; i < matrix_size; i++)
 	{
 		result += arrayA[i] * arrayB[i][column];
 	}
@@ -102,29 +106,59 @@ int dotProduct(int(&arrayA)[sizeRowA], int(&arrayB)[sizeRowB][sizeColumnB], int 
 }
 
 
-
-template <size_t sizeRowA, size_t sizeRowB, size_t sizeColumnB>
-int* multiplyStripe(int(&arrayA)[sizeRowA], int(&arrayB)[sizeRowB][sizeColumnB])
+int* multiplyStripe(int* arrayA, int** arrayB, int matrix_size)
 {
-	int* array_stripe_C = new int[sizeColumnB];
+	int* array_stripe_C = new int[matrix_size];
 
-	for(int i = 0; i < sizeColumnB; i++)
+	for(int i = 0; i < matrix_size; i++)
 	{
-		array_stripe_C[i] = dotProduct(arrayA, arrayB, i);
+		array_stripe_C[i] = dotProduct(arrayA, arrayB, i, matrix_size);
 	}
 
 
 	return array_stripe_C;
 }
 
- int(&fillarr(int(&arr)[5]))[5]{ // no decay; argument must be size 5
-	 return arr;
- }
+int malloc2dint(int ***array, int n, int m) {
+
+	/* allocate the n*m contiguous items */
+	int *p = (int *)malloc(n*m*sizeof(int));
+	if (!p) return -1;
+
+	/* allocate the row pointers into the memory */
+	(*array) = (int **)malloc(n*sizeof(int*));
+	if (!(*array)) {
+		free(p);
+		return -1;
+	}
+
+	/* set up the pointers into the contiguous memory */
+	for (int i = 0; i<n; i++)
+		(*array)[i] = &(p[i*m]);
+
+	return 0;
+}
+
+int free2dint(int ***array) 
+{
+	/* free the memory - the first element of the array is at the start */
+	free(&((*array)[0][0]));
+
+	/* free the pointers into the memory */
+	free(*array);
+
+	return 0;
+}
+
 
 // function that generates a matrix in binary form on disk
-void generateMatrixFile() {
+template <typename Type>
+void generateMatrixFile(Type** arr, int matrix_size) 
+{
 	// generate a matrix of values that need to be written to disk in the form of a one dimensional array this will write out an 8x8 matrix
-	double matrix[64] = {
+	/*
+	double matrix[64] = 
+	{
 		1, 1, 1, 1, 1, 1, 1, 1,
 		1, 1, 1, 1, 1, 1, 1, 1,
 		1, 1, 1, 1, 1, 1, 1, 1,
@@ -134,97 +168,220 @@ void generateMatrixFile() {
 		1, 1, 1, 1, 1, 1, 1, 1,
 		1, 1, 1, 1, 1, 1, 1, 1
 	};
-
-	// open up a file for writing
-	/*
-	FILE *output;
-	fopen_s(&output, "matrix.dat", "wb");
 	*/
 
+	// convert to 1d array
+	double* p = new double[matrix_size*matrix_size];
+
+	for (int i = 0; i < matrix_size; i++)
+	{
+		for (int j = 0; j < matrix_size; j++)
+		{
+			p[i * matrix_size + matrix_size] = arr[i][j];
+			std::cout << "p[i * matrix_size + matrix_size] " << p[i * matrix_size + matrix_size] << " " << std::endl;
+		}
+	}
+
+	// open up a file for writing
+	FILE *output;
+	fopen_s(&output, "matrix.txt", "wb");
+	
+	/*
 	FILE *output = fopen("matrix.dat", "wb");
-	if (!output) {
+	*/
+
+	if (!output) 
+	{
 		return;
 	}
 
+
+	std::cout << "generateMatrixFile sizeof(Type) " << sizeof(double) << " ";
+	std::cout << "generateMatrixFile matrix_size*matrix_size " << matrix_size*matrix_size << " " << std::endl;
 	// do a simple fwrite to write the matrix to file
-	fwrite(matrix, sizeof(double), 64, output);
+	fwrite(p, sizeof(double), matrix_size*matrix_size, output);
 
 
 	// close the file when we are finished writing
 	fclose(output);
 
-	printMatrixFromFile();
+	// printMatrix(p);
 
 }
 
 
 /* master node method */
-void coordinator(int world_size)
+void coordinator(int world_size, int matrix_size)
 {
-	int arr[8][8];
-	int arr2[8][8];
-
-
+/*	// A complete
+	int **arr = new int*[matrix_size];
+	for (int i = 0; i < matrix_size; i++)
+	{
+		arr[i] = new int[matrix_size];
+	}
+	// B complete
+	int **arr2 = new int*[matrix_size];
+	for (int w = 0; w < matrix_size; w++)
+	{
+		arr2[w] = new int[matrix_size];
+	}
+	// C complete
+	int **arr3 = new int*[matrix_size];
+	for (int r = 0; r < matrix_size; r++)
+	{
+		arr3[r] = new int[matrix_size];
+	}
 	
+	// fill
 	initIntMatrix(arr2);
 	initIntMatrix(arr);
 
-	arr2[0][0] = 5; // testing
+	int size_for_each_node = matrix_size / world_size;
+	int matrix_size_1d = matrix_size * matrix_size;
 
+	int **partition = new int*[size_for_each_node];
+	for (int r = 0; r < size_for_each_node; r++)
+	{
+		arr3[r] = new int[matrix_size];
+	}
+
+
+	// matrix size broadcast
+	MPI_Bcast(&matrix_size_1d, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	// broadcast the partition size / stripe size
+	MPI_Bcast(&size_for_each_node, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	// broadcast size of an individual row 
+	MPI_Bcast(&matrix_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	*/
+
+	int **array;
+	malloc2dint(&array, matrix_size, matrix_size);
+
+	initIntMatrix(array, matrix_size);
+
+	// scatter A
+	// scatter the partition to each node
+	// MPI_Scatter(arr, size_for_each_node, MPI_INT, partition, size_for_each_node,
+	//	MPI_INT, 0, MPI_COMM_WORLD);
+
+
+	// Broadcast B
+	MPI_Bcast(&(array[0][0]), matrix_size*matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
+	
+	std::cout << "node: " << world_rank << std::endl;
+	printMatrix(array);
+	//printMatrix(arr);
+	std::cout << std::endl;
+/*
+
+	// print A and B
 	printMatrix(arr);
 	printMatrix(arr2);
-
-	//int dotproduct = dotProduct_two_dimensional(arr, 0, arr2, 0);
-	// std::cout << dotproduct << " ";
-
-
-	int arr3[8][8];
-	int* result = new int[8];
-
-	for (int i = 0; i < 8; i++)
+	
+	// results from stripes
+	int* result = new int[matrix_size];
+	 
+	for (int i = 0; i < matrix_size; i++)
 	{
 		result = multiplyStripe(arr[i], arr2);	
-		for (int j = 0; j < sizeof(result); j++)
+		for (int j = 0; j < matrix_size; j++)
 		{
 			arr3[i][j] = result[j];
 		}
 	}
-
-	printMatrix(arr3);
 	
+	// print the result
+	 printMatrix(arr3);*/
+	// generateMatrixFile(arr);
 
-
-
-	
-
+	// printMatrixFromFile(arr, matrix_size);
 
 	
 }
 
 /* slave node method */
 void participant()
-{
+{	
+	/*
+	int matrix_size_1d;
+	int size_for_each_node;
 	
+	*/
+	/*
+	// matrix size broadcast
+	MPI_Bcast(&matrix_size_1d, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	// broadcast the partition size / stripe size
+	MPI_Bcast(&size_for_each_node, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	
+
+	std::cout << "matrix_size " << matrix_size << " " << std::endl;
+
+	// B complete matrix
+	int **arr2 = new int*[matrix_size];
+	for (int w = 0; w < matrix_size; w++)
+	{
+		arr2[w] = new int[matrix_size];
+	}
+	// part of A
+	int **arr = new int*[size_for_each_node];
+	for (int i = 0; i < size_for_each_node; i++)
+	{
+		arr[i] = new int[matrix_size];
+	}
+	// part of C
+	int **arr3 = new int*[size_for_each_node];
+	for (int r = 0; r < size_for_each_node; r++)
+	{
+		arr3[r] = new int[matrix_size];
+	}
+	*/
+
+	int matrix_size;
+	// broadcast size of an individual row 
+	MPI_Bcast(&matrix_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	int **array;
+	malloc2dint(&array, matrix_size, matrix_size);
+
+	// scatter A 
+	// MPI_Scatter(arr, size_for_each_node, MPI_INT, arr3, size_for_each_node,
+	//	MPI_INT, 0, MPI_COMM_WORLD);
+
+	// broadcast B
+	MPI_Bcast(&(array[0][0]), matrix_size*matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+	std::cout << "node: " << world_rank << std::endl;
+	printMatrix(array);
+	//printMatrix(arr);
+	std::cout << std::endl;
+
+
 }
 
 int main(int argc, char** argv) 
 {
 	MPI_Init(NULL, NULL);
 	int world_size;
+	matrix_size = 16;
 
 
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
 	/*  get the matricies */
-	// sscanf_s(argv[1], "%d", &iterations); 
+	sscanf_s(argv[1], "%d", &matrix_size);
 
 	
 	// master node 
 	if(world_rank == 0) 
 	{
+		std::cout << "matrix size: " << matrix_size << std::endl;
 		auto start_time = MPI_Wtime();
-		coordinator(world_size);
+		coordinator(world_size, matrix_size);
 		auto end_time = MPI_Wtime();
 		// output the time
 		std::cout << "time in s:" << end_time-start_time << std::endl;
@@ -242,5 +399,20 @@ int main(int argc, char** argv)
 
 
 
-
+/*// returns the dot product of two int matricies
+template <size_t sizeRowA, size_t sizeColumnA, size_t sizeRowB, size_t sizeColumnB>
+int dotProduct_two_dimensional(int(&matrixA)[sizeRowA][sizeColumnA], int row, int(&matrixB)[sizeRowB][sizeColumnB], int column)
+{
+	// only works if the size of the row of A is equal to size of column of B
+	if(sizeRowA != sizeColumnB)
+	{
+		return EXIT_FAILURE;
+	}
+	int result = 0;
+	for (size_t i = 0; i < sizeColumnA; i++)
+	{
+		result += matrixA[row][i] * matrixB[i][column];
+	}
+	return result;
+}*/
 
